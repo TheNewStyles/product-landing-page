fe02eeb598ab65b5b6aaa5bb4b73eda3b1198ee5
TheNewStyles <tyler.peterson@utah.edu>
2018-06-21 15:39:01 -0500

    Add basic elements. Added all to App.js TODO to break them into components

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import logo from '../images/logo.svg';
+import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <header id="header">
+          <div>
+            <img id="header-img" src={logo} className="App-logo" alt="logo for blah" />
+          </div>          
+          <nav id="nav-bar">
+            <ul>
+              <li><a className="nav-link" href="google.com">Home</a></li>
+              <li><a className="nav-link" href="google.com">About</a></li>
+              <li><a className="nav-link" href="google.com">Contact</a></li>
+            </ul>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section>
+          <article>
+            <form id="form" action="">
+              <label htmlFor="email">THE BEST STUFF SIGN UP HERE</label>
+              <input id="email" name="email" type="email" placeholder="Enter email" required/>
+              <input id="submit" name="submit" type="button" action="https://www.freecodecamp.com/email-submit" value="Submit" />
+            </form>  
+          </article>
+          <article>
+            <div>
+              <img id="header-img" src={logo} className="App-logo" alt="logo for blah" />
+              <h2>Fast Shipping</h2>
+              <p>We make sure you recieve your trombone as soon as we have finished making it. We also provide free returns if you are not satisfied.</p>
+            </div>
+            <div>
+              <img id="header-img" src={logo} className="App-logo" alt="logo for blah" />
+              <h2>Fast Shipping</h2>
+              <p>We make sure you recieve your trombone as soon as we have finished making it. We also provide free returns if you are not satisfied.</p>
+            </div>
+            <div>
+              <img id="header-img" src={logo} className="App-logo" alt="logo for blah" />
+              <h2>Fast Shipping</h2>
+              <p>We make sure you recieve your trombone as soon as we have finished making it. We also provide free returns if you are not satisfied.</p>
+            </div>
+          </article>
+          <article>
+            <video width="620" controls
+              poster="https://upload.wikimedia.org/wikipedia/commons/e/e8/Elephants_Dream_s5_both.jpg" >
+              <source
+                src="https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4"
+                type="video/mp4" />
+              <source
+                src="https://archive.org/download/ElephantsDream/ed_hd.ogv"
+                type="video/ogg" />
+              <source
+                src="https://archive.org/download/ElephantsDream/ed_hd.avi"
+                type="video/avi" />
+              Your browser doesn't support HTML5 video tag.
+            </video>
+          </article>
+          <article>
+            <div>
+              <h3>Some info</h3>
+              <p>Lorem ipsum</p>
+              <input type="button" value="See details"/>
+            </div>
+            <div>
+              <h3>Some info</h3>
+              <p>Lorem ipsum</p>
+              <input type="button" value="See details"/>
+            </div>
+            <div>
+              <h3>Some info</h3>
+              <p>Lorem ipsum</p>
+              <input type="button" value="See details"/>
+            </div>
+          </article>
+        </section>  
       </div>
     );
   }
