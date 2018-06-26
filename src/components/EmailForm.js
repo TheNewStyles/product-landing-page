056029ac06a833cfc1396c34f8d6d6f2c7d6f56f
TheNewStyles <tyler.peterson@utah.edu>
2018-06-26 13:40:17 -0500

    Update Readme and typo

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -8,7 +8,7 @@ class EmailForm extends Component {
     return (
         <article className="dark-purple-txt">
           <form id="form" action="" className={this.props.className}>
-              <h4><label htmlFor="email">Subscribe To Muffin Top Bakery's Email List</label></h4>
+              <h4><label htmlFor="email">Subscribe To Muffin Tops Bakery's Email List</label></h4>
               <input id="email" name="email" className="email-form-input" type="email" placeholder="Enter email" required/>
               <ProductButton id="submit" className="productbutton-email-button" name="submit" action="https://www.freecodecamp.com/email-submit" value="Get Sweet Deals!"/>
           </form>
